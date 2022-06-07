71b077639df2344f05b4707872df9d0442e94345
Jay Thanki <thankijay99@gmail.com>
2022-06-07 16:57:25 +0530

    Created google strategy and user model

--- backend/server.ts
+++ backend/server.ts
@@ -1,10 +1,16 @@
 import express, { Express, Request, Response } from "express";
 import dotenv from 'dotenv';
 import colors from 'colors';
+import passport from 'passport';
+import session from 'express-session';
 import { connectDB } from './config/db';
+import { router as auth } from './routes/auth';
 
 // Load Config
 dotenv.config();
+
+// passport config
+require('./config/passport')(passport);
 
 const app: Express = express();
 
@@ -12,9 +18,27 @@ connectDB();
 
 const PORT = process.env.PORT;
 
+// Init Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+//Routes
 app.get('/', (req: Request, res: Response) => {
   res.send("Hello");
 });
+
+app.use('/auth', auth);
+
+// Sessions
+app.use(session({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: false
+}))
+
+// Passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
 
 
 app.listen(PORT, () => console.log(colors.yellow(`Server running on port ${PORT}`)));
