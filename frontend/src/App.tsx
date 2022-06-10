b1689b17f2fb5350a6ee0778db65026c152ba4a4
Jay Thanki <thankijay99@gmail.com>
2022-06-10 18:35:10 +0530

    Tried to add google oauth 2.0 login on the frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,19 +1,25 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { GoogleOAuthProvider } from '@react-oauth/google';
 import Login from './components/auth/Login';
 import Home from './Home';
 
 import './App.css';
 
-function App() {
+const CLIENTID = "918908597696-fu9rsapoqiih191od2j4ignlsg5v3ef2.apps.googleusercontent.com";
+
+const App = () => {
+  
   return (
     <div className="App">
-      <Router>
-        <Routes>
-          <Route path='/' element={<Login />} />
-          <Route path='/home' element={<Home />} />
-        </Routes>
-      </Router>
+      <GoogleOAuthProvider clientId={CLIENTID}>
+        <Router>
+          <Routes>
+            <Route path='/' element={<Login />} />
+            <Route path='/home' element={<Home />} />
+          </Routes>
+        </Router>
+      </GoogleOAuthProvider>
     </div>
   );
 }
--- frontend/src/components/auth/Login.tsx
+++ frontend/src/components/auth/Login.tsx
@@ -1,13 +1,22 @@
 import React from 'react';
 import axios from 'axios';
+import { GoogleLogin } from '@react-oauth/google';
 
 
 const Login = () => {
 
-  const onGoogle = async () => {
-    axios.create({ withCredentials: true});
-    const res = await axios.get('/auth/google');
-  }
+  // const onGoogle = async () => {
+  //   axios.create({ withCredentials: true});
+  //   const res = await axios.get('/auth/google');
+  // }
+
+  // const googleSucess = async (res: GoogleLoginResponse | GoogleLoginResponseOffline) => {
+  //   console.log(res);
+  // };
+
+  // const googleFailure = (error: any) => {
+  //   console.log(error);
+  // };
   
   return (
     <div>
@@ -20,7 +29,23 @@ const Login = () => {
                   <h1 className="text-center mb-3"><i className="bi bi-book" style={{"marginRight": "10px"}}></i>Your reads</h1>
                   <p className='fs-5'>Pick, organize, and mark your reads.</p>
                   <hr />
-                  <button className="btn btn-primary btn-lg btn-block mt-4" onClick={onGoogle}><i className="bi bi-google" style={{"marginRight": "10px"}}></i>Log In With Google</button>
+                  <GoogleLogin 
+                    onSuccess={async (credentialResponse) => {
+                      const accessToken = credentialResponse.credential;
+                      let config = {
+                        headers: {
+                          Authorization: `Bearer ${accessToken}`
+                        }
+                      }
+                      const res = await axios.get("https://people.googleapis.com/v1/people/me?personFields=names HTTP/1.1", config);
+                      console.log(res);
+                      // console.log(credentialResponse);
+                    }}
+                    onError={() => {
+                      console.log('Login Failed');
+                    }}
+                  />
+                  {/* <button className="btn btn-primary btn-lg btn-block mt-4" onClick={onGoogle}><i className="bi bi-google" style={{"marginRight": "10px"}}></i>Log In With Google</button> */}
                 </div>
               </div>
             </div>
