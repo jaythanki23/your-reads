2b842dbb3e9edfe999409f6c33d84ced8d968dda
Jay Thanki <thankijay99@gmail.com>
2022-08-03 14:14:39 +0530

    Deploy

--- backend/config/db.ts
+++ backend/config/db.ts
@@ -4,7 +4,7 @@ import { ConnectOptions } from "mongoose";
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI!, {
+    const conn = await mongoose.connect(process.env.MONGO_URI as string, {
       useNewUrlParser: true,
         // useCreateIndex: true,
       useUnifiedTopology: true
